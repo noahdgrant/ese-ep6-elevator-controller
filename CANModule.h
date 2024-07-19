36267b65002d2ee667806caa4d80a66326d5a2bf
Noah Grant <105687410+noahdgrant@users.noreply.github.com>
2024-07-19 16:04:05 -0400

    Update floor setpoints and setpoint tolerance (#10)
    * Update floor setpoints and setpoint tolerance
    
    * Fixed CAN init bug

--- CANModule.h
+++ CANModule.h
@@ -30,9 +30,10 @@
 #define A 1.5                               // Linear Gain on the difference measurement
 #define MINHEIGHT 100                       // Below this height the Elevator Stops moving - SOFTWARE KILL SWITCH
 #define MAXHEIGHT 1500                      // Above this height the Elevator stops moving - SOFTWARE KILL SWITCH
-#define FLOOR1_SP 350                       // Floor 1 setpoint 300mm from bottom (sensor)
-#define FLOOR2_SP 635                       // Floor 2 setpoint 700mm from bottom (sensor)
-#define FLOOR3_SP 1220                      // Floor 3 setpoint 1000mm from bottom (sensor)
+#define FLOOR1_SP 350                       // Floor 1 setpoint from bottom (sensor)
+#define FLOOR2_SP 635                       // Floor 2 setpoint from bottom (sensor)
+#define FLOOR3_SP 1220                      // Floor 3 setpoint from bottom (sensor)
+#define SETPOINT_TOLERANCE 20               // in mm
 // Sets the care/don't care bits in the ID (11 bits long for Standard CAN) [first 4 nibbles] and first two bytes of data [last four nibbles]. Using this mask we care about all ID bits so that ID of a message must match a filter below.    
 #define MASK 0x07FF0000                    // Mask for filters
 #define FILTER_SC 0x01000000                // Acceptance filter for ID 0x100 (Supervisory Controller - Raspberry Pi)
